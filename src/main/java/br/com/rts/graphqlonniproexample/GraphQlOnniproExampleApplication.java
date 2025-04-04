9528d28c083a5b513cb4a6ca32070024a1d9b26d
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2025-04-04 10:24:11 -0400

    feat: Create a new project

---
+++ src/main/java/br/com/rts/graphqlonniproexample/GraphQlOnniproExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.rts.graphqlonniproexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlOnniproExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlOnniproExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rts/graphqlonniproexample/GraphQlOnniproExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rts.graphqlonniproexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlOnniproExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
